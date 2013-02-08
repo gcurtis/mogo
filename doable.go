3d52ec3e1c97805add3128406d1867f344190930
Greg Curtis <greg.r.curtis@gmail.com>
2013-02-08 02:13:00 -0800

    Properly handles function calls with nil values

--- doable.go
+++ doable.go
@@ -21,7 +21,13 @@ func (this *doable) run(params ...interface{}) R {
 
 	in := make([]reflect.Value, len(params))
 	for i, p := range params {
-		in[i] = reflect.ValueOf(p)
+		if p == nil {
+			// Reflect f's ith param to obtain a nil value for it.
+			t := v.Type().In(i)
+			in[i] = reflect.Zero(t)
+		} else {
+			in[i] = reflect.ValueOf(p)
+		}
 	}
 
 	out := v.Call(in)
